f27e0cfebb5390ae75262f356b958d353d85c157
Nordin <135022193+NordinCoding@users.noreply.github.com>
2025-03-03 13:22:09 +0100

    making changes

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from mainPlay import bol_scraper, log_to_file
 from urllib.parse import unquote
@@ -12,11 +12,10 @@ async def read_root():
     return {'Correct usage: "http://157.180.29.91/URL"'}
 
 
-@app.get("/scrape/<path:mypath>")
-def scrape_item(mypath):
+@app.get("/scrape")
+def scrape_item(url: str = Query(..., description="URL of the product to scrape")):
     try:
-        URL = mypath
-        dictValues = bol_scraper(URL)
+        dictValues = bol_scraper(url)
         return dictValues
     except Exception as e:
         log_to_file(f"Failed to scrape item: {str(e)}", "ERROR")
