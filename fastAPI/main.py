c615270656af72a9f714894bdea67caf532486e5
Nordin <135022193+NordinCoding@users.noreply.github.com>
2025-03-03 12:28:57 +0100

    Pushing finished version of API

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,16 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from mainPlay import bol_scraper, log_to_file
 
 app = FastAPI()
-
-
-class Product(BaseModel):
-    URL: str
-    name: str
-    ogPrice: float
-    currentPrice: float
-
 
 
 @app.get("/")
@@ -18,11 +11,7 @@ async def read_root():
     return {"Hello, World!"}
 
 
-@app.get("/items/{product_id}")
-async def read_item(product_id: int, q: Union[str, None] = None):
-    return {"item_id": product_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, product: Product):
-    return {"product_name": product.name, "ogPrice": product.ogPrice, "currentPrice": product.currentPrice}
+@app.get("/items/{URL}")
+def scrape_item(URL: str):
+    dictValues = bol_scraper(URL)
+    return dictValues
