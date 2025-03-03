f89f40cac891e2f65d8538ae5ea9204be1a40a4e
Nordin <135022193+NordinCoding@users.noreply.github.com>
2025-03-03 13:07:57 +0100

    updating main.py

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,17 +1,23 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from mainPlay import bol_scraper, log_to_file
+from urllib.parse import unquote
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello, World!"}
+    return {'Correct usage: "http://157.180.29.91/URL"'}
 
 
-@app.get("/items/{URL}")
-def scrape_item(URL: str):
-    dictValues = bol_scraper(URL)
-    return dictValues
+@app.get("/scrape/<path:mypath>")
+def scrape_item(mypath):
+    try:
+        URL = unquote(mypath)
+        dictValues = bol_scraper(URL)
+        return dictValues
+    except Exception as e:
+        log_to_file(f"Failed to scrape item: {str(e)}", "ERROR")
+        raise HTTPException(status_code=500, detail="Failed to scrape item")
